378784819e2ab10e453f8798d637f80ab38800db
Jahangir Ripon <ripon@rtbd.com>
2018-08-04 16:19:52 +0600

    Initial commit

---
+++ resources/views/albums/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <h2>Index</h2>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PhotoShow</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.css">
+</head>
+<body>
+    <div class="container">
+
+    	@yield('content')
+    	
+    </div>
+</body>
+</html>
